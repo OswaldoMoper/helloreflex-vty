c5ed4d540c524aed18dc5dcc84d067bc897098b9
MOPER <oswaldomoyap@gmail.com>
2025-05-30 23:47:24 +0000

    add hello-reflex-vty project modules

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,21 @@
-{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Reflex
-import Reflex.Vty
+import           Data.Text    (pack)
+import qualified Graphics.Vty as V
+import           Reflex
+import           Reflex.Vty
 
 main :: IO ()
-main = runVtyApp $ \vty -> do
-    text "Hello, Reflex-VTY"
+main = mainWidget $ initManager_ $ do
+  text (pure (pack "¡Hello, Reflex-VTY!"))
+
+  inp <- input
+  let quitEvent = fforMaybe inp $ \case
+        V.EvKey V.KEsc [] -> Just ()
+        V.EvKey (V.KChar 'c') [V.MCtrl] -> Just ()
+        _ -> Nothing
+
+  return quitEvent
