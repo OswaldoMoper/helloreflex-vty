3313354ecc15f3e748c0b9d94f67c403d893ba3b
MOPER <oswaldomoyap@gmail.com>
2025-05-30 21:42:27 +0000

    add nix modules

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-compat, flake-parts, haskell-flake, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [ inputs.haskell-flake.flakeModule ];
+      perSystem = { self', system, pkgs, ... }: {
+        haskellProjects.default = {
+          basePackages = pkgs.haskell.packages.ghc96;
+          devShell = {
+            enable = true;
+            tools = hp: {
+              inherit (hp) cabal-install haskell-language-server;
+            };
+          };
+      };
+      packages.default = self'.packages.hello-reflex-vty;
+      apps.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/hello-reflex-vty";
+      };
+    };
+  };
+}
