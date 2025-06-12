3437232a311944a8f8cd00ed682f4bc2d1a442f3
MOPER <oswaldomoyap@gmail.com>
2025-06-12 01:43:35 +0000

    Using recursiveDo and rec in resizeEntity

--- app/resizeEntity.hs
+++ app/resizeEntity.hs
@@ -1,13 +1,14 @@
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE RecursiveDo       #-}
 
 module Main where
 
+import Control.Monad.Fix (MonadFix)
 import qualified Graphics.Vty as V
-import Reflex
-import Reflex.Vty
+import           Reflex
+import           Reflex.Vty
 
 main :: IO ()
 main = mainWidget $ initManager_ $ do
@@ -19,33 +20,46 @@ main = mainWidget $ initManager_ $ do
         _ -> Nothing
   return quitEvent
 
-resizeRectangle :: (HasDisplayRegion t m, HasImageWriter t m, HasTheme t m, PerformEvent t m, MonadHold t m, TriggerEvent t m) 
+resizeRectangle :: (HasDisplayRegion t m, HasImageWriter t m, HasTheme t m, PerformEvent t m, MonadHold t m, TriggerEvent t m, MonadFix m, MonadSample t (Performable m))
                 => Event t V.Event -> m ()
-resizeRectangle inp = mdo
-  let initialX = 10
+resizeRectangle inp = do
+  let lText = "¡Hello, Reflex-VTY!"
+      initialX = 10
       initialY = 5
-      initialWidth = 16
-
+      initialWidth = max 16 (length lText + 2)
       mouseUpEvent = fmapMaybe (\case
         V.EvMouseUp{} -> Just ()
         _ -> Nothing) inp
-      mouseStopDragging = fmap (const False) mouseUpEvent
-  hDyn <- holdDyn 5 $ heightChange inp
-  isDragging <- holdDyn False $ leftmost [mouseStartDragging initialX initialY hDyn initialWidth inp, mouseStopDragging]
-  let heightEventFiltered = gate (current isDragging) $
-        fmapMaybe (\case
-          V.EvMouseDown _ y _ _ -> Just y
-          _ -> Nothing) inp
-      heightChange
-        = attachPromptlyDynWith
-            (\ h y -> y - initialY + h) hDyn heightEventFiltered
-  tellImages $ fmap (\h -> [drawRect initialX initialY initialWidth h]) (current hDyn)
+  rec
+  -- TODO: Code to handle resizing
+  -- We need a way to resize rectangle dynamically
+  -- For now, we will keep the width constant
+  -- and allow height to change when the user clicks and drags
+  -- the vertical edges of the rectangle.
+    currentHDyn <- foldDyn const 5 $
+      attachPromptlyDynWith (\_baseHeight newHeight -> newHeight) baseHeightDyn heightEvent
+    baseHeightDyn <- foldDyn const 5 $
+      tag (current currentHDyn) mouseUpEvent
+    let mouseStartResizing =
+          True <$ attachPromptlyDynWithMaybe
+            (\h ev -> case ev of
+                V.EvMouseDown x y _ _ | isOnEdge x y h -> Just ()
+                _ -> Nothing)
+            baseHeightDyn
+            inp
+          where
+            isOnEdge x y h = y == initialY + h - 1 && x >= initialX && x < initialX + initialWidth
+        mouseStopResizing = fmap (const False) mouseUpEvent
+    isResizing <- holdDyn False $ leftmost [mouseStartResizing, mouseStopResizing]
+    let heightEvent = gate (current isResizing) $
+          fmapMaybe (\case
+            V.EvMouseDown x y _ _ -> Just (max 3 (y - initialY + 1))
+            _ -> Nothing) inp
+  tellImages $ fmap (\h -> [drawRect initialX initialY initialWidth h lText]) (current currentHDyn)
 
-
-drawRect :: Int -> Int -> Int -> Int -> V.Image
-drawRect x y w h =
-  let lText = "Hello World"
-      topBottom = V.string V.defAttr ("╭" ++ replicate (w - 2) '─' ++ "╮")
+drawRect :: Int -> Int -> Int -> Int -> String -> V.Image
+drawRect x y w h lText =
+  let topBottom = V.string V.defAttr ("╭" ++ replicate (w - 2) '─' ++ "╮")
       emptyRow = V.string V.defAttr ("│" ++ replicate (w - 2) ' ' ++ "│")
       bottomRow = V.string V.defAttr ("╰" ++ replicate (w - 2) '─' ++ "╯")
 
@@ -57,19 +71,3 @@ drawRect x y w h =
       rowsBefore = replicate textRowPosition emptyRow
       rowsAfter = replicate (h - 3 - textRowPosition) emptyRow
   in V.translate x y $ V.vertCat ([topBottom] ++ rowsBefore ++ [textRow] ++ rowsAfter ++ [bottomRow])
-
-mouseStartDragging initialX initialY hDyn initialWidth inp =
-  True <$ attachPromptlyDynWith (\h ev -> fmapMaybe (\case
-    V.EvMouseDown x y _ _ -> 
-      if (y == initialY + h - 1) && (x > initialX && x < initialX + initialWidth) 
-      then Just True
-      else Nothing
-    _ -> Nothing) (Just ev)) hDyn inp
-
--- mouseStartDragging initialX initialY initialHeight initialWidth inp = 
---   True <$ fmapMaybe (\case
---     V.EvMouseDown x y _ _ -> 
---       if (y == initialY + initialHeight - 1) && (x > initialX && x < initialX + initialWidth) then
---         Just y
---       else Nothing
---     _ -> Nothing) inp
